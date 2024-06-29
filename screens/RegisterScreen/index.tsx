faee6e0d11bc75de13afb56aed476c52ab1f7f50
victor <motoggb979@gmail.com>
2024-06-29 19:56:59 -0300

    Criação da tela de Registro

--- screens/RegisterScreen/index.tsx
+++ screens/RegisterScreen/index.tsx
@@ -1,106 +1,140 @@
 import React, { useEffect, useRef, useState } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import { View, Text, StyleSheet, Alert, Image, TextInput } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 
 import Button from "../../components/Button";
-import Input, { InputProps } from "../../components/Input";
-import { TextInput } from "react-native-gesture-handler";
+import Input from "../../components/Input";
+import { RegisterUserAsync, loginUserAsync } from "../../database";
+import Toast from "react-native-toast-message";
+import { useAuth } from "../../context/AuthContext";
+import { FormProvider, useForm } from "../../context/FormContext";
 
-type RootStackParamList = {
+type TypeRootStackParamList = {
   Login: undefined;
   Home: undefined;
+  Register: undefined;
 };
 
-type RegisterScreenNavigationProp = StackNavigationProp<RootStackParamList, "Login">;
+type RegisterScreenNavigationProp = StackNavigationProp<TypeRootStackParamList, "Register">;
 
-type Props = {
+export type TypePropsNavigation = {
   navigation: RegisterScreenNavigationProp;
 };
 
-function RegisterScreen({ navigation }: Props) {
-  const [fieldValidity, setFieldValidity] = useState<{ [key: string]: boolean | undefined }>({});
-
-  const [user, setUser] = useState("");
+function RegisterScreen({ navigation }: TypePropsNavigation) {
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const { isLoggedIn, setIsLoggedIn } = useAuth();
+  const { isFormValid } = useForm();
 
   // Refs para os campos de entrada
   const passwordRef = useRef<TextInput>(null);
   const emailRef = useRef<TextInput>(null);
 
   // Função para simular o clique do botão "Cadastrar"
-  const handleRegisterPress = () => {
-    alert("Botão pressionado!");
-  };
-
-  const handleLogin = async () => {
-    try {
-      const response = await fetch('http://seu-endereco-da-api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
+  const handleRegisterPress = async () => {
+    if (!isFormValid) {
+      Toast.show({
+        type: "error",
+        text1: "Campos inválidos",
+        text2: "Confira os campos e tente novamente.",
+        visibilityTime: 4000,
+        autoHide: true,
       });
 
-      const data = await response.json();
+      return;
+    }
+    try {
+      const registrado = await RegisterUserAsync({ email, password, name });
 
-      if (response.status === 200) {
-        // Login bem-sucedido
-        Alert.alert("Sucesso", "Logado com sucesso!");
+      if (registrado) {
+        Toast.show({
+          type: "success",
+          position: "top",
+          text1: "Registro bem-sucedido",
+          text2: "Você foi logado com sucesso!",
+          visibilityTime: 4000,
+          autoHide: true,
+          topOffset: 30,
+        });
+
+        setIsLoggedIn(isLoggedIn);
+        navigation.navigate("Home");
       } else {
-        // Falha no login
-        Alert.alert("Erro", data.message);
+        Toast.show({
+          type: "error",
+          text1: "Falha ao Registrar",
+          text2: "Email ou senha incorretos. Por favor, tente novamente.",
+          visibilityTime: 4000,
+          autoHide: true,
+        });
       }
     } catch (error) {
-      console.error(error);
-      Alert.alert("Erro", "Não foi possível conectar à API");
+      setIsLoggedIn(false);
+      Toast.show({
+        type: "error",
+        text1: "Erro",
+        text2: "Ocorreu um erro ao tentar fazer o registro. Por favor, tente novamente mais tarde.",
+        visibilityTime: 4000,
+        autoHide: true,
+      });
+
+      console.log(error);
     }
   };
 
   useEffect(() => {}, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Login Screen</Text>
+    <FormProvider>
       <View style={styles.container}>
-        <Input
-          placeholder=""
-          label="Usuário"
-          id="usuario"
-          required={false}
-          value={user}
-          onChangeText={(text) => setUser(text)}
-          returnKeyType="next"
-          onSubmitEditing={() => passwordRef.current?.focus()}
-        />
-        <Input
-          placeholder=""
-          label="Senha"
-          id="senha"
-          required={true}
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          type="password"
-          returnKeyType="next"
-          onSubmitEditing={() => emailRef.current?.focus()}
-          ref={passwordRef}
-        />
-        <Input
-          placeholder=""
-          label="Email"
-          id="email"
-          required={true}
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-          returnKeyType="done"
-          onSubmitEditing={handleRegisterPress}
-          ref={emailRef}
-        />
-        <Button title="Cadastrar" onPress={handleRegisterPress} type="salvar" />
+        <View style={styles.container}>
+          <Image source={require("../../assets/images/logo.png")} style={styles.logo} />
+          <Input
+            placeholder=""
+            label="Usuário"
+            id="usuario"
+            required={true}
+            value={name}
+            onChangeText={(text) => setName(text)}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current?.focus()}
+          />
+          <Input
+            placeholder=""
+            label="Senha"
+            id="senha"
+            required={true}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            type="password"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current?.focus()}
+            ref={passwordRef}
+          />
+          <Input
+            placeholder=""
+            label="Email"
+            id="email"
+            required={true}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            returnKeyType="done"
+            onSubmitEditing={handleRegisterPress}
+            ref={emailRef}
+          />
+          <View style={styles.containerBottom}>
+            <Button
+              title="Cadastrar"
+              type="salvar"
+              onPress={handleRegisterPress}
+              style={{ minWidth: "70%", maxWidth: "90%" }}
+            />
+          </View>
+        </View>
       </View>
-      <Button title="Login" onPress={() => navigation.navigate("Home")} />
-    </View>
+    </FormProvider>
   );
 }
 
@@ -110,6 +144,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#dadada",
+  },
+  logo: {
+    width: 250,
+    height: 250,
+    marginBottom: 5,
+  },
+  containerBottom: {
+    minWidth: "70%",
+    maxWidth: "90%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
